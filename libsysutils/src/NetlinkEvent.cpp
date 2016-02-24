71f4da2a9b0946b1dfc3aeff769eff9723691ac8
ZION959 <ziontran@gmail.com>
2016-02-24 03:08:23 -0700

    revert: IPA tether: support for netlink event from ipa
    IPA will send ascii netlink broadcast and subsystem is net, not
    qlog, it's necessary to change the subsystem to qlog here
    
    Change-Id: Id11cd9597d3defa22bcd3f029abc32c673156d86 (reverted from commit 03aecc79d29499699e9e1dd99ed156c58e460fd4)

--- libsysutils/src/NetlinkEvent.cpp
+++ libsysutils/src/NetlinkEvent.cpp
@@ -626,10 +626,6 @@ bool NetlinkEvent::parseAsciiNetlinkMessage(char *buffer, int size) {
         }
         s += strlen(s) + 1;
     }
-    if(findParam("ALERT_NAME") !=NULL ) {
-        mSubsystem = strdup("qlog");
-        mAction = NlActionChange;
-    }
     return true;
 }
 
